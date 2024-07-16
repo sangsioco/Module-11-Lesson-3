f8aa5c2e1afcea0a7732be5bc576e537b171c356
sangsioco <sangsioco@gmail.com>
2024-07-16 13:49:55 -0700

    updated hash key

--- marvel-comics/src/components/characterlist.jsx
+++ marvel-comics/src/components/characterlist.jsx
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
+
 
 const CharacterList = ({ onCharacterClick }) => {
   const [characters, setCharacters] = useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios.get('https://gateway.marvel.com/v1/public/characters?ts=1&apikey=c642fb55c89c4eb1fcf4617dffa6770e&hash=200e12ef7b86e3be28d79f838b4aebdf')
+    axios.get('https://gateway.marvel.com/v1/public/characters?ts=1&apikey=c642fb55c89c4eb1fcf4617dffa6770e&hash=6cb106ea5b1bb397b28b1c7482a26d5a')
       .then(response => {
         setCharacters(response.data.data.results);
       })
@@ -22,14 +23,15 @@ const CharacterList = ({ onCharacterClick }) => {
   }
 
   return (
-    <div className="character-list">
+    <div className="grid-container">
       {characters.map(character => (
-        <div key={character.id} onClick={() => onCharacterClick(character.id)}>
+        <div key={character.id} className="card" onClick={() => onCharacterClick(character.id)}>
           <img 
             src={`${character.thumbnail.path}.${character.thumbnail.extension}`} 
             alt={`${character.name} thumbnail`} 
+            className="thumbnail"
           />
-          <p>{character.name}</p>
+          <p className="name">{character.name}</p>
         </div>
       ))}
     </div>
